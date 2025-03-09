f8a8789c75a4219d289162368159e59a6896754c
19smabtahinoor <abtahi.educxy@gmail.com>
2025-03-09 20:44:51 +0600

    gg

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
+import { Analytics } from '@vercel/analytics/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { ThemeProvider } from './lib/theme';
 import './index.css';
+import { ThemeProvider } from './lib/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <ThemeProvider>
       <App />
     </ThemeProvider>
